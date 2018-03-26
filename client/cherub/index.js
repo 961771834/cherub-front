bf6d38b21983ab00edf8325bf663e0fb7967379e
leon.zheng <961771834@qq.com>
2018-03-27 00:11:48 +0800

    mobx-demo

--- client/cherub/index.js
+++ client/cherub/index.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {observable, action} from 'mobx';
+import {observer} from 'mobx-react';
+
+var appState = observable({
+    timer: 0
+});
+
+
+appState.resetTimer = action(function reset() {
+    appState.timer = 0;
+});
+
+setInterval(action(function tick() {
+    appState.timer += 1;
+}), 1000);
+
+
+@observer
+class TimerView extends React.Component {
+    render() {
+        return (<button onClick={this.onReset.bind(this)}>
+                Seconds passed: {this.props.appState.timer}
+            </button>);
+    }
+
+    onReset () {
+        this.props.appState.resetTimer();
+    }
+};
+
 
 const element =(<h1>Hello, cherub!</h1>);
-ReactDOM.render(element,document.getElementById("app"))
+ReactDOM.render(<TimerView appState={appState} />,document.getElementById("app"))
 
